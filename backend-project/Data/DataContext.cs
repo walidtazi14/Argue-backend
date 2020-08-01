3a8339ade43738e94a307f4d5b5edf11c6d85ca1
Tazi <walid.tazi@accenture.com>
2020-08-01 17:35:38 +0100

    Add project files.

---
+++ backend-project/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using backend_project.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend_project.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) :base(options)
+        {
+        }
+
+        public DbSet<Value> Values  { get; set; }
+
+    }
+}
